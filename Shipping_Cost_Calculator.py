a75a348326fd3ab0be11f190689a44f6a2267941
Aadithya <biju.aadithya1@gmail.com>
2024-07-26 20:25:59 +0530

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -9,6 +9,7 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
-# Here is a new update by GitHub bijuaadithya
+# Here is a new update by bijuaadithya
+# Here is another update by bijuaadithya
 
 
